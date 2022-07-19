d00fa02ee5edaedef5c4191cf266ad358bec982b
Kahmoanpooh Thongwat <65552281+korn-git@users.noreply.github.com>
2022-07-19 14:07:17 +0700

    First commit

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,6 @@
 FROM python:3.9  
-
 WORKDIR /app
-
 COPY ./requirements.txt ./requirements.txt
-
 RUN pip install --no-cache-dir --upgrade -r ./requirements.txt
-
-# 
 COPY ./main.py ./main.py
-
-# 
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "5555"]
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import datetime
 import jwt
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 from fastapi.middleware.cors import CORSMiddleware
+import requests
+import pyrebase
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -16,9 +19,27 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+config = {
+    "apiKey": "AIzaSyDAQuB3pKyB3M9VFZESKNFUpANNUBYFaN8",
+    "authDomain": "user-password-storage.firebaseapp.com",
+    "projectId": "user-password-storage",
+    "storageBucket": "user-password-storage.appspot.com",
+    "messagingSenderId": "593789962772",
+    "databaseURL": "",
+    "appId": "1:593789962772:web:852d387622ddd7dc6cb501"
+}
 
-@app.get("/api/")
-async def create_token(key: str, secret: str):
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
+
+class Account(BaseModel):
+    email: str
+    password: str
+
+
+@app.post("/api/getToken/")
+async def create_token(account: Account, response: Response):
     def get_expired_datetime():
         """Generate expired datetime for JWT token
             return: expired datetime in unix timestamp"""
@@ -26,14 +47,35 @@ async def create_token(key: str, secret: str):
         unix_expired_time = datetime.datetime.timestamp(expired_time)
         return(unix_expired_time)
 
-    expired_datetime = get_expired_datetime()
-    key = key
-    secret = secret
-    encoded = jwt.encode(
-        {"exp": expired_datetime},
-        secret,
-        algorithm="HS256",
-        headers={"iss": key},
+    """Firebase username password validation"""
+    try:
+        auth.sign_in_with_email_and_password(account.email, account.password)
+        """request key and secret using email"""
+        response = requests.post(
+            f'http://kong:8001/consumers/{account.email}/jwt')
 
-    )
-    return {"token": encoded}
+        key = response.json().get('key')
+        secret = response.json().get('secret')
+        """Generate Token"""
+        expired_datetime = get_expired_datetime()
+        encoded = jwt.encode(
+            {"exp": expired_datetime},
+            secret,
+            algorithm="HS256",
+            headers={"iss": key},
+        )
+        response.status_code = status.HTTP_200_OK
+        return {
+            "token": encoded
+        }
+    except Exception as e:
+        if str(e) == "sign_in_with_email_and_password() missing 1 required positional argument: 'password'":
+            response.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
+            return {
+                "message": "Email or password is missing"
+            }
+        else:
+            response.status_code = status.HTTP_401_UNAUTHORIZED
+            return {
+                "message": "Wrong email or password"
+            }
